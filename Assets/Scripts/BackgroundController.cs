5ffe876af50e85a52e468ceeb390591c8dd60ff9
kykaroo <boris_nemcev@mail.ru>
2023-08-14 23:54:08 +0500

    Добавлены элементы окружения

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -1,19 +1,31 @@
+using System;
 using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private GameObject moon;
+    public float moonspeedX = 0.99f;
+    public float moonspeedY = 0.01f;
+    [SerializeField] private float foreGroundSpeedX = 0.001f;
+    
     public bool needMove;
+
     private void Update()
     {
         if (!needMove) return;
-        
-        MoveBackground();
+
+        var moveDelta = Time.deltaTime * speed;
+        var moveDeltaGround = new Vector3(-moveDelta, 0, 0);
+        var moveDeltaMoon = new Vector3(moveDelta * moonspeedX, -(moveDelta * moonspeedY), 0);
+
+        MoveBackground(moveDeltaGround, moveDeltaMoon);
     }
 
-    private void MoveBackground()
+    private void MoveBackground(Vector3 moveDeltaGround, Vector3 moveDeltaMoon)
     {
-        transform.Translate(new(-(Time.deltaTime * speed),0,0));
+        transform.Translate(moveDeltaGround);
+        moon.transform.Translate(moveDeltaMoon);
     }
 
     internal void ResetBackground()
